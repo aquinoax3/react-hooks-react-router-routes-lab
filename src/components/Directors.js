92beeb679cf672f472c690c038f01152ddd118cc
Axel Aquino <aquino.axel@gmail.com>
2022-03-08 01:10:30 -0500

    Completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,26 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const listDirectors = directors.map((director) => {
+    return (
+      <div>
+        <h3>Name: {director.name}</h3>
+        <p>Movies:</p>
+        <ul>
+          {director.movies.map((movie) => {
+            return <li>{movie}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {listDirectors}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,27 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const listMovies = movies.map((movie) => {
+    return (
+      <div>
+        <h1>Title: {movie.title}</h1>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map((e) => {
+            return <li>{e}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {listMovies}
+    </div>
+  );
 }
 
 export default Movies;
